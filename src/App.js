78ec4f0f16d3249f208395ebbc72e2bfa027bb6f
Kaio Silveira - Desenvolvimento DSB <devell28@GRMLTCRISP.LOCAL>
2019-04-12 21:16:50 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const users = [
+  { id: 1, name: 'Takeshi' },
+  { id: 2, name: 'Goiaba' },
+  { id: 3, name: 'Kaio' },
+  { id: 4, name: 'Errera' },
+]
+
+
+
+const Title = ({text}) => (
+  <h1>{text}</h1>
+)
+
+const Card = ({index, text, button}) => (
+  <div className='card'>
+    {index}{text}<button>{button}</button>
+  </div> 
+)
+
+const Form = () =>{
+  const [name, setName] = useState('')
+
+  return  (
+    <form className="form">
+      <input value={name} onChange={e => setName(e.target.value)} placeholder="name" />
+      <button>+</button>
+    </form> 
+  )
+
 }
 
-export default App;
+const App = () => (
+  <div className="App">
+    <Title text='Usuários'/> 
+    <Card text="#Kaio" index="#1" button="x"/>
+    {
+      users.map(n => (<Card text={n.name} index={n.id} button="x"/>))
+    }
+    <Form></Form>
+  </div>
+)
+
+const Add = () =>{
+
+
+
+}
+
+
+
+
+
+export default App
---
+++ src/Qualquer.js
@@ -0,0 +1,5 @@
+export const fn = () => console.log('Olá')
+
+const otherFn = () => console.log('Outro olá')
+
+export default otherFn
